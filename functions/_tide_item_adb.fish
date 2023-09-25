cac93159b23a4db6e64c3d1766274f0808a9bbe0
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-09-26 00:02:10 +0800

    update: show product
    Signed-off-by: fengqiyang <fengqiyang@xiaomi.com>

--- functions/_tide_item_adb.fish
+++ functions/_tide_item_adb.fish
@@ -1,5 +1,10 @@
 function _tide_item_adb
     if test -n "$ANDROID_SERIAL"
-        _tide_print_item adb $tide_adb_icon' ' $ANDROID_SERIAL
+        set -l detail (adb devices -l | rg $ANDROID_SERIAL | string replace -rf '(\S+).*product:(\S+).*' '$1 $2' | string trim)
+        if test -z "$detail"
+            _tide_print_item adb $tide_adb_icon' ' "$ANDROID_SERIAL offline"
+        else
+            _tide_print_item adb $tide_adb_icon' ' $detail
+        end
     end
 end
